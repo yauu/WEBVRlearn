401a0bd7319e003a3ab3baa3c016270a5364d4c1
yauu <a5988aaa@gmail.com>
2021-07-29 14:26:19 +0800

    add

---
+++ data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-202001",
+      "name": "2020智慧教育高峰會(松菸一號倉)01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6110060742847265,
+          "pitch": 0.14777521079037115,
+          "rotation": 0,
+          "target": "1-04"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0009385455908645923,
+          "pitch": -0.28960054500635657,
+          "title": "TITLEtest",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.7336204087467184,
+          "pitch": -0.32977083062902857,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-04",
+      "name": "南門壹陸04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};

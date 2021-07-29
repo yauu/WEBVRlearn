3de4f551d63468d06980f965680a70d03a21ad0e
yauu <a5988aaa@gmail.com>
2021-07-29 16:01:56 +0800

    5

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-202001",
-      "name": "2020智慧教育高峰會(松菸一號倉)01",
+      "id": "0-04",
+      "name": "南門壹陸04",
       "levels": [
         {
           "tileSize": 256,
@@ -26,60 +26,27 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.6110060742847265,
-          "pitch": 0.14777521079037115,
+          "yaw": -2.4743924177075307,
+          "pitch": 0.612187444191397,
           "rotation": 0,
-          "target": "1-04"
+          "target": "0-04"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.0009385455908645923,
-          "pitch": -0.28960054500635657,
-          "title": "標題12313",
-          "text": "內文中文english"
-        },
-        {
-          "yaw": 1.7336204087467184,
-          "pitch": -0.32977083062902857,
+          "yaw": 2.910666385219848,
+          "pitch": -0.02779941855535739,
           "title": "Title",
-          "text": "Text"
+          "text": "<img src=\"https://cdn.ourcodeworld.com/public-media/articles/articleocw-5c3e422596525.png\" >"
         }
       ]
-    },
-    {
-      "id": "1-04",
-      "name": "南門壹陸04",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 960,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
     }
   ],
-  "name": "VR網站測試",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
